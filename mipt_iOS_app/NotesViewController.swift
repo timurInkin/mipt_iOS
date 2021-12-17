73bb5633dcc853e6844a314a328b69c64cd92576
ms30 <ms30@unused.ms30.sim-networks.net>
2021-12-17 01:25:53 +0100

    third init

--- mipt_iOS_app/NotesViewController.swift
+++ mipt_iOS_app/NotesViewController.swift
@@ -6,10 +6,91 @@
 //
 
 import UIKit
-
-class NotesViewController: UIViewController {
-    
-
-
+extension NotesViewController: DataControllerDelegate {
 
 }
+    
+
+class NotesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var notesTable: UITableView!
+    @IBOutlet weak var noNotes: UILabel!
+    
+    
+    var notes = [Note]()
+    let dataController = DataController()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dataController.delegate = self
+        dataController.getNotes()
+        // Do any additional setup after loading the view.
+    }
+
+//
+//    @IBAction func composeButtonTapped(_ sender: Any) {
+//        addNote()
+//    }
+    
+    func deleteNoteAt(index: Int) {
+        notes.remove(at: index)
+        
+    }
+    
+//    func addNote() {
+//        let note = "note \(notes.count + 1)"
+//
+//        notes.append(note)
+//        notesTable.reloadData()
+//
+//    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let notesCount = notes.count
+        if notesCount > 0 {
+             noNotes.isHidden = true
+         } else {
+             noNotes.isHidden = false
+         }
+             return notesCount
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NoteCell")! as UITableViewCell
+        let note = notes[indexPath.row]
+        cell.textLabel?.text = note.text
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            deleteNoteAt(index: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 10.0
+    }
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView()
+    }
+    func dataSourceChanged(dataSource: [Note]?, error:Error?) {
+        if let notes = dataSource {
+            self.notes = notes
+            notesTable.reloadData()
+        }
+        
+    }
+}
+
+        
+
